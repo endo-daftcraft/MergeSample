32bbaf4bbd7810f6cbe63e548c4bcd823ef463b4
M.Endo <endo@daftcraft.co.jp>
2022-11-17 15:30:56 +0900

    add code

---
+++ Assets/Scripts/SamplePresenter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Daftcraft
+{
+    public class SamplePresenter : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _buttonShow;
+        [SerializeField]
+        private Button _buttonHide;
+
+        [SerializeField]
+        private Animator _animator;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _buttonShow.onClick.AddListener( OnClick_Show );
+            _buttonHide.onClick.AddListener( OnClick_Hide );
+        }
+
+        private void OnClick_Show()
+        {
+            _animator?.SetFloat( "speed", 1f );
+            _animator?.SetBool( "visible", true );
+        }
+
+        private void OnClick_Hide()
+        {
+            _animator?.SetFloat( "speed", 1f );
+            _animator?.SetBool( "visible", false );
+            _animator?.Play( "Ofuda_anim_hide" );
+        }
+    }
+}
